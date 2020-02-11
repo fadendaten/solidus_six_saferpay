8c3a88a2fd7882d6c2244dd2dbff29b3cc9e3797
Simon Kiener <jugglinghobo@gmail.com>
2020-02-11 16:09:22 +0100

    fixed asset precompile initializer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
-Rails.application.config.assets.precompile << 'solidus_six_saferpay_manifest.rb'
+if Rails.application.config.respond_to?(:assets)
+  Rails.application.config.assets.precompile << 'solidus_six_saferpay_manifest.js'
+end
