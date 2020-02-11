9ba7aba5f7f06154ed7606389ad9cb195dd55de2
Simon Kiener <jugglinghobo@gmail.com>
2020-02-11 14:42:36 +0100

    regenerated dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_170670) do
+ActiveRecord::Schema.define(version: 2020_02_11_134087) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
