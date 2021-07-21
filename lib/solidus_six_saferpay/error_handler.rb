5e93fb258519812f0c8375779d6516b8e153ac3f
Simon Kiener <jugglinghobo@gmail.com>
2021-07-21 17:10:59 +0200

    updated error_handler spec to match rubocop style

--- lib/solidus_six_saferpay/error_handler.rb
+++ lib/solidus_six_saferpay/error_handler.rb
@@ -8,15 +8,14 @@ module SolidusSixSaferpay
       Rails.logger.send(level, error)
 
       SolidusSixSaferpay.config.error_handlers.each do |handler|
-        if !handler.respond_to?(:call)
+        begin
+          handler.call(error, level: level)
+        rescue StandardError
           Rails.logger.warn("SolidusSixSaferpay::Configuration ERROR: The" \
                             "attached error handler #{handler} can not be called" \
                             "with #{handler}.call(error, level: level)")
-
-          # skip to next handler
           next
         end
-        handler.call(error, level: level)
       end
     end
   end
--- spec/solidus_six_saferpay/error_handler_spec.rb
+++ spec/solidus_six_saferpay/error_handler_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 module SolidusSixSaferpay
   RSpec.describe ErrorHandler do
-    let(:error) { StandardError.new }
     let(:error_handlers) { [] }
+
+    let(:error_handler_class) do
+      Class.new do
+        def initialize(name)
+          @name = name
+        end
+
+        def to_s
+          @name
+        end
+
+        def call(error, _level: :error)
+          error
+        end
+      end
+    end
 
     before do
       allow(SolidusSixSaferpay.config).to receive(:error_handlers).and_return(error_handlers)
@@ -11,51 +26,73 @@ module SolidusSixSaferpay
 
     describe '.handle' do
       it 'defaults to level :error' do
-        expect(Rails.logger).to receive(:error).with(error)
-        described_class.handle(error)
+        allow(Rails.logger).to receive(:error)
+
+        described_class.handle(StandardError.new)
+
+        expect(Rails.logger).to have_received(:error).with(StandardError.new)
       end
 
       it 'allows for configuring the error level' do
-        expect(Rails.logger).to receive(:info).with(error)
-        described_class.handle(error, level: :info)
+        allow(Rails.logger).to receive(:info)
+
+        described_class.handle(StandardError.new, level: :info)
+
+        expect(Rails.logger).to have_received(:info).with(StandardError.new)
       end
 
       context 'when any attached handler can not receive our error messages' do
-        let(:error_handler1) { double("Handler1", to_s: 'handler1', call: true) }
-        let(:error_handler2) { double("Handler2", to_s: 'handler2') }
-        let(:error_handler3) { double("Handler3", to_s: 'handler3', call: true) }
+        let(:misconfigured_error_handler_class) do
+          Class.new do
+            def initialize(name)
+              @name = name
+            end
 
-        let(:error_handlers) { [error_handler1, error_handler2, error_handler3] }
-
-        before do
-          allow(error_handler1).to receive(:respond_to?).with(:call).and_return(true)
-          allow(error_handler2).to receive(:respond_to?).with(:call).and_return(false)
-          allow(error_handler3).to receive(:respond_to?).with(:call).and_return(true)
+            def to_s
+              @name
+            end
+          end
         end
+
+        let(:error_handler2) { misconfigured_error_handler_class.new('handler2') }
+        let(:error_handler3) { error_handler_class.new('handler3') }
+
+        let(:error_handlers) { [error_handler2, error_handler3] }
 
         it 'informs about the misconfiguration via Rails logger' do
-          expect(Rails.logger).to receive(:warn).with(/ERROR:.*handler2.*/)
+          allow(Rails.logger).to receive(:warn)
+          described_class.handle(StandardError.new)
 
-          described_class.handle(error)
+          expect(Rails.logger).to have_received(:warn).with(/ERROR:.*handler2.*/)
         end
 
-        it 'skips to the next error handler' do
-          expect(error_handler1).to receive(:call).with(error, level: :error)
-          expect(error_handler2).not_to receive(:call)
-          expect(error_handler3).to receive(:call).with(error, level: :error)
+        it 'does not fail when calling a misconfigured handler' do
+          allow(error_handler2).to receive(:call).and_call_original
 
-          described_class.handle(error)
+          described_class.handle(StandardError.new)
+
+          expect(error_handler2).to have_received(:call)
+        end
+
+        it 'calls other error handlers after the misconfigured one' do
+          allow(error_handler3).to receive(:call)
+
+          described_class.handle(StandardError.new)
+
+          expect(error_handler3).to have_received(:call).with(StandardError.new, level: :error)
         end
       end
 
       context 'when an attached handler can receive our error messages' do
-        let(:error_handler) { double("CustomErrorHandler", call: true) }
+        let(:error_handler) { error_handler_class.new('handler') }
         let(:error_handlers) { [error_handler] }
 
         it 'forwards the error to the error handler' do
-          expect(error_handler).to receive(:call).with(error, level: :error)
+          allow(error_handler).to receive(:call)
 
-          described_class.handle(error)
+          described_class.handle(StandardError.new)
+
+          expect(error_handler).to have_received(:call).with(StandardError.new, level: :error)
         end
       end
     end
