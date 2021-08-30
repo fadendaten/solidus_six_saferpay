20fd82541d2df1352515543996a4ad50d6e31697
Simon Kiener <jugglinghobo@gmail.com>
2021-08-30 16:39:15 +0200

    disable rubocop linelength warning in generator

--- lib/generators/solidus_six_saferpay/install/install_generator.rb
+++ lib/generators/solidus_six_saferpay/install/install_generator.rb
@@ -11,8 +11,8 @@ module SolidusSixSaferpay
       end
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_six_saferpay\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_six_saferpay\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_six_saferpay\n" # rubocop:disable Layout/LineLength
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_six_saferpay\n" # rubocop:disable Layout/LineLength
       end
 
       def add_stylesheets
