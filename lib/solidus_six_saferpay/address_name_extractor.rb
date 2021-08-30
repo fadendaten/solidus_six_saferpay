9eb251321a0e914bb7e3fcd2ff63c16a8fee29fc
Simon Kiener <jugglinghobo@gmail.com>
2021-08-30 17:54:41 +0200

    handle NameError for Solidus < 2.11 if Address::Name is not defined

--- lib/solidus_six_saferpay/address_name_extractor.rb
+++ lib/solidus_six_saferpay/address_name_extractor.rb
@@ -21,9 +21,6 @@ module SolidusSixSaferpay
     private
 
     def extract_names(address)
-      # Address::Name has been introduced in 2.11
-      return address if Spree.solidus_gem_version <= Gem::Version.new('2.10')
-
       if address.respond_to?(:first_name)
         Spree::Address::Name.new(address.first_name, address.last_name)
       elsif address.respond_to?(:firstname)
@@ -33,6 +30,11 @@ module SolidusSixSaferpay
       else
         raise "Unable to safely extract first- and lastname from address"
       end
+    rescue NameError => e
+      # Address::Name has been introduced in 2.11
+      return address if Spree.solidus_gem_version <= Gem::Version.new('2.10')
+
+      throw e
     end
   end
 end
