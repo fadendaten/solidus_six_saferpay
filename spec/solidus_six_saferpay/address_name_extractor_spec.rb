4506e9a25448e2db50ddd73bc9ca6072df5676b0
Simon Kiener <jugglinghobo@gmail.com>
2021-08-31 14:59:35 +0200

    refactored address name extractor spec

--- spec/solidus_six_saferpay/address_name_extractor_spec.rb
+++ spec/solidus_six_saferpay/address_name_extractor_spec.rb
@@ -4,117 +4,81 @@ module SolidusSixSaferpay
   RSpec.describe AddressNameExtractor do
     subject(:service) { described_class.new(address) }
 
-    context 'when solidus uses a combined name in address' do
-      let(:address) { double('Spree::Address', id: 1, name: 'John Von Doe') }
-
-      before do
-        allow(SolidusSupport).to receive(:combined_first_and_last_name_in_address?).and_return(true)
-      end
-
-      context 'when Address implementation supports #first_name and #last_name' do
-        before do
-          allow(address).to receive(:first_name).and_return('John')
-          allow(address).to receive(:last_name).and_return('Von Doe')
-        end
-
-        describe '#first_name' do
-          it 'respects these attributes' do
-            expect(service.first_name).to eq('John')
-          end
-        end
-
-        describe '#last_name' do
-          it 'respects these attributes' do
-            expect(service.last_name).to eq('Von Doe')
-          end
-        end
-      end
-
-      context 'when Address implementation supports #firstname and #lastname' do
-        before do
-          allow(address).to receive(:firstname).and_return('John')
-          allow(address).to receive(:lastname).and_return('Von Doe')
-        end
-
-        describe '#first_name' do
-          it 'respects these attributes' do
-            expect(service.first_name).to eq('John')
-          end
-        end
-
-        describe '#last_name' do
-          it 'respects these attributes' do
-            expect(service.last_name).to eq('Von Doe')
-          end
-        end
-      end
-
-      context 'when Address implementation does not support seperate first- and lastname attributes' do
-        before do
-          allow(address).to receive(:name).and_return('John Von Doe')
-        end
-
-        context 'when unsafe name extraction is enabled' do
-          before do
-            allow(SolidusSixSaferpay.config).to receive(:allow_unsafe_address_name_extraction).and_return(true)
-          end
-
-          describe '#first_name' do
-            it 'is extracted from name' do
-              expect(service.first_name).to eq('John')
-            end
-          end
-
-          describe '#last_name' do
-            it 'is extracted from name' do
-              expect(service.last_name).to eq('Von Doe')
-            end
-          end
-
-        end
-
-        context 'when unsafe name extraction is disabled' do
-          before do
-            allow(SolidusSixSaferpay.config).to receive(:allow_unsafe_address_name_extraction).and_return(false)
-          end
-
-          describe '#first_name' do
-            it 'throws an error' do
-              expect { service.first_name }.to raise_error(AddressNameExtractor::UnsafeNameExtractionError)
-            end
-          end
-
-          describe '#last_name' do
-            it 'throws an error' do
-              expect { service.first_name }.to raise_error(AddressNameExtractor::UnsafeNameExtractionError)
-            end
-          end
-        end
+    let(:address) do
+      if SolidusSupport.combined_first_and_last_name_in_address?
+        create(:address, name: 'John Von Doe')
+      else
+        create(:address, first_name: 'John', last_name: 'Von Doe')
       end
     end
 
-    context 'when solidus does not use a combined name in address' do
-      let(:address) { double('Spree::Address') }
-
+    context 'when Address has first_name and last_name attributes' do
       before do
-        allow(SolidusSupport).to receive(:combined_first_and_last_name_in_address?).and_return(false)
+        allow(address).to receive(:first_name).and_return('Jon')
+        allow(address).to receive(:last_name).and_return('Van Doe')
       end
 
       describe '#first_name' do
-        it 'is delegated to given address instance' do
-          allow(address).to receive(:first_name)
-          service.first_name
-
-          expect(address).to have_received(:first_name)
+        it 'respects custom Address#first_name attribute' do
+          expect(service.first_name).to eq('Jon')
         end
       end
 
       describe '#last_name' do
-        it 'is delegated to given address instance' do
-          allow(address).to receive(:last_name)
-          service.last_name
+        it 'respects custom Address#last_name attribute' do
+          expect(service.last_name).to eq('Van Doe')
+        end
+      end
+    end
 
-          expect(address).to have_received(:last_name)
+    context 'when Address has firstname and lastname attributes' do
+      before do
+        allow(address).to receive(:first_name).and_return('Johnny')
+        allow(address).to receive(:last_name).and_return('Doe')
+      end
+
+      it 'respects custom Address#firstname attribute' do
+        expect(service.first_name).to eq('Johnny')
+      end
+
+      it 'respects custom Address#lastname attribute' do
+        expect(service.last_name).to eq('Doe')
+      end
+    end
+
+    context 'when unsafe name extraction from combined names is enabled' do
+      before do
+        allow(SolidusSixSaferpay.config).to receive(:allow_unsafe_address_name_extraction).and_return(true)
+      end
+
+      describe '#first_name' do
+        it 'extracts first name from combined name attribute' do
+          expect(service.first_name).to eq('John')
+        end
+      end
+
+      describe '#last_name' do
+        it 'extracts last name from combined name attribute' do
+          expect(service.last_name).to eq('Von Doe')
+        end
+      end
+    end
+
+    context 'when unsafe name extraction from combined names is disabled' do
+      before do
+        allow(SolidusSupport).to receive(:combined_first_and_last_name_in_address?).and_return(true)
+        allow(SolidusSixSaferpay.config).to receive(:allow_unsafe_address_name_extraction).and_return(false)
+      end
+
+      describe '#first_name' do
+        it 'throws an error' do
+          expect { service.first_name }.to raise_error(AddressNameExtractor::UnsafeNameExtractionError)
+        end
+      end
+
+      describe '#last_name' do
+        it 'throws an error' do
+          expect { service.last_name }.to raise_error(AddressNameExtractor::UnsafeNameExtractionError)
         end
       end
     end
